31b4e869e8554280b2186c576079a108aad38a77
spelexander <spelexander93@gmail.com>
2022-08-22 09:22:48 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# depster
+# depste
+
+```bash
+
+$ depster --root ./frontend --src ./frontend/src
+🔬  ./frontend/package.json found
+🔬  scanning: ./frontend/src
+📦  current packages: 54 / 15 dev
+📦  used packages: 28 / 1 dev
+📦  proposed changes:
+{
+  "dependencies": {
+    "@apollo/client": "^3.3.6",
+    "@material-ui/core": "^4.11.0",
+    "@material-ui/icons": "^4.9.1",
+    "@material-ui/lab": "^4.0.0-alpha.51",
+    "@material-ui/styles": "^4.9.10",
+    "@react-firebase/auth": "^0.2.10",
+    "@react-hook/debounce": "^3.0.0",
+    "@testing-library/jest-dom": "^4.2.4",
+    "@testing-library/react": "^9.3.2",
+    "@testing-library/user-event": "^7.1.2",
+    "@types/dotenv": "^8.2.0",
+    "@types/jest": "^24.0.0",
+    "@types/lodash.clone": "^4.5.6",
+    "@types/lodash.get": "^4.4.6",
+    "@types/lodash.omit": "^4.5.6",
+    "@types/lodash.remove": "^4.7.6",
+    "@types/lodash.set": "^4.3.6",
+    "@types/node": "^13.9.1",
+    "@types/react": "^16.9.34",
+    "@types/react-dom": "^16.9.0",
+    "@types/react-highlight-words": "^0.16.1",
+    "@types/react-router-dom": "^5.1.5",
+    "dotenv": "^8.2.0",
+    "firebase": "^8.2.1",
+    "fuse.js": "^6.4.6",
+    "graphql": "^15.3.0",
+    "lighten-darken-color": "^1.0.0",
+    "lodash": "^4.17.21",
+    "lodash.clone": "^4.5.0",
+    "lodash.get": "^4.4.2",
+    "lodash.omit": "^4.5.0",
+    "lodash.remove": "^4.7.0",
+    "lodash.set": "^4.3.2",
+    "material-ui-chip-input": "^1.1.0",
+    "material-ui-popup-state": "^1.7.2",
+    "notistack": "^1.0.3",
+    "prop-types": "^15.7.2",
+    "react": "^16.12.0",
+    "react-device-detect": "^1.12.0",
+    "react-dom": "^16.12.0",
+    "react-google-button": "^0.7.1",
+    "react-highlight-words": "^0.17.0",
+    "react-hook-form": "^6.13.1",
+    "react-horizontal-scrolling-menu": "^0.7.8",
+    "react-media": "^1.10.0",
+    "react-router-dom": "^5.2.0",
+    "react-scripts": "3.4.0",
+    "react-scroll-horizontal": "^1.6.6",
+    "react-sweet-state": "^2.4.4",
+    "react-use-poll": "^1.1.2",
+    "react-uuid": "^1.0.2",
+    "string-to-color": "^2.2.2",
+    "typescript": "^3.8.3",
+    "use-clipboard-hook": "^1.1.1"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.12.3",
+    "@storybook/addon-actions": "^6.0.27",
+    "@storybook/addon-essentials": "^6.0.27",
+    "@storybook/addon-links": "^6.0.27",
+    "@storybook/node-logger": "^6.0.27",
+    "@storybook/preset-create-react-app": "^3.1.4",
+    "@storybook/react": "^6.0.27",
+    "@testing-library/dom": "^7.0.4",
+    "@typescript-eslint/parser": "^2.24.0",
+    "babel-loader": "8.0.6",
+    "babel-plugin-module-resolver": "^4.1.0",
+    "eslint": "^6.8.0",
+    "husky": "^5.0.6",
+    "prettier": "2.2.1",
+    "react-is": "^17.0.1"
+    "@storybook/react": "^6.0.27"
+  }
+}
+```
---
+++ src/main.rs
@@ -0,0 +1,175 @@
+extern crate clap;
+
+use std::collections::{BTreeMap, HashSet};
+
+use std::fs;
+use std::format;
+use std::fs::{metadata};
+use serde::{Deserialize, Serialize};
+use clap::{Arg, App};
+use regex::Regex;
+use prettydiff::diff_lines;
+
+const FILE_NAME: &str = "package.json";
+
+#[derive(Serialize, Deserialize)]
+#[allow(non_snake_case)]
+struct Package {
+    dependencies: BTreeMap<String, String>,
+    devDependencies: BTreeMap<String, String>,
+}
+
+pub fn path_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
+}
+
+fn main() {
+    let matches = App::new("depster")
+        .version("0.0.2")
+        .author("spelexander")
+        .about("Organise and optimise your package.json packages with depster")
+        .arg(Arg::with_name("root")
+            .short("r")
+            .long("root")
+            .help("Root directory path containing package.json file, defaults to ./")
+            .takes_value(true))
+        .arg(Arg::with_name("src")
+            .short("s")
+            .long("src")
+            .help("Directory path containing the input source files, defaults to ./src")
+            .takes_value(true))
+        .arg(Arg::with_name("ext")
+            .short("e")
+            .long("ext")
+            .help("File extensions to scan, defaults to (.tsx|.ts|.jsx|.js|.mjs|.cjs)")
+            .takes_value(true))
+        .get_matches();
+
+    let root = matches.value_of("root").unwrap_or(".");
+    let src = matches.value_of("src").unwrap_or("./src");
+    let extension_matcher = matches.value_of("ext").unwrap_or(r"(\.tsx$|\.ts$|\.jsx$|\.js$|\.mjs$|\.cjs$)");
+    let extension_matcher = Regex::new(&extension_matcher).expect("Invalid regex provided");
+
+    let package_json_path = root.ends_with(FILE_NAME).then(|| root.to_owned()).unwrap_or(format!("{}/{}", &root, FILE_NAME));
+
+    if !path_exists(&package_json_path) {
+        panic!("{}/{} not found, doing nothing.", &root, FILE_NAME);
+    } else if !path_exists(&src) {
+        panic!("{} not found, doing nothing.", &src);
+    }
+
+    println!("🔬  {}/{} found", &root, FILE_NAME);
+    println!("🔬  scanning: {}", &src);
+
+    let package: String = fs::read_to_string(package_json_path).expect("Unable to find file");
+    let package: Package = serde_json::from_str(&package).expect("Unable to read file. Is it json?");
+
+    println!("📦  current packages: {} / {} dev", package.dependencies.keys().len(), package.devDependencies.keys().len());
+
+    let deps: HashSet<String> = package.dependencies
+        .keys()
+        .cloned()
+        .collect();
+
+    let dev_deps: HashSet<String> = package.devDependencies
+        .keys()
+        .cloned()
+        .collect();
+
+
+    let mut all_deps = HashSet::new();
+    for dep in &deps {
+        all_deps.insert(dep.to_owned());
+    }
+    for dev_dep in &dev_deps {
+        all_deps.insert(dev_dep.to_owned());
+    }
+
+    // Scan dist files for dev dependencies
+    let result = scan_files(&src, &extension_matcher, &all_deps);
+    let mut new_deps: BTreeMap<String, String> = BTreeMap::new();
+    let mut new_dev_deps: BTreeMap<String, String> = BTreeMap::new();
+
+    for dep in result {
+        if dev_deps.contains(&*dep) {
+            // dev dep from the start
+            new_dev_deps.insert(dep.to_owned(), package.devDependencies.get(&*dep).unwrap().to_owned());
+        } else if dep.starts_with("@types") {
+            // should be in dev
+            new_dev_deps.insert(dep.to_owned(), package.dependencies.get(&*dep).unwrap().to_owned());
+        } else {
+            // normal dep
+            new_deps.insert(dep.to_owned(), package.dependencies.get(&*dep).unwrap().to_owned());
+        }
+    }
+
+    println!("📦  used packages: {} / {} dev", &new_deps.len(), &new_dev_deps.len());
+
+    let new_package = Package {
+        dependencies: new_deps,
+        devDependencies: new_dev_deps,
+    };
+
+    let old_package = Package {
+        dependencies: package.dependencies,
+        devDependencies: package.devDependencies,
+    };
+
+    let input= serde_json::to_string_pretty(&old_package).unwrap();
+    let output= serde_json::to_string_pretty(&new_package).expect("Could not serialise output json");
+
+    println!("📦  proposed changes:");
+    println!("{}", diff_lines(&input, &output));
+}
+
+fn scan_files(path: &str, matcher: &Regex, dep_list: &HashSet<String>) -> HashSet<String> {
+
+    let mut found_deps: HashSet<String> = HashSet::new();
+    let mut check_list: HashSet<String> = dep_list.clone();
+
+    // loop over every file/dir and dive deeper or scan for deps
+    for source_path in fs::read_dir(path).expect("scanned directory does not exist!") {
+        if check_list.len() == 0 {
+            break;
+        }
+
+        let entry = source_path.unwrap();
+        let file_name = entry
+            .path()
+            .file_name()
+            .unwrap()
+            .to_string_lossy()
+            .into_owned();
+        let file_path = format!("{}/{}", &path, &file_name);
+
+        let meta = metadata(entry.path()).unwrap();
+
+        if meta.is_dir() {
+            // recursively explore files in sub directories
+            let sub_found: HashSet<String> = scan_files(&file_path, matcher, &check_list);
+            for dep in sub_found {
+                found_deps.insert(dep.to_owned());
+                check_list.remove(&*dep);
+            }
+            continue;
+        }
+
+
+        // if not a valid file extension
+        if !matcher.is_match(&file_name) {
+            continue;
+        }
+
+        let file_content: String = fs::read_to_string(file_path).expect("Unable to find file");
+
+        // if it's a match find specific dep and remove it from the master list
+        for dep in check_list.clone() {
+            if file_content.contains(&*dep) {
+                found_deps.insert(dep.to_owned());
+                check_list.remove(&*dep);
+            }
+        }
+    }
+
+    return found_deps;
+}
