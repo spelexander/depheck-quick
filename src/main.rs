e278109b5a3edcd50cdc506068a10263014ca684
spelexander <spelexander93@gmail.com>
2022-08-24 21:39:49 +1000

    Use jwalk instead of read dir

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 extern crate clap;
 
 use std::collections::{BTreeMap, HashSet};
-
+use std::time::Instant;
 use std::fs;
 use std::format;
-use std::fs::{metadata};
 use serde::{Deserialize, Serialize};
 use clap::{Arg, App};
 use regex::Regex;
 use prettydiff::diff_lines;
+use jwalk::{WalkDir};
 
 const FILE_NAME: &str = "package.json";
 
@@ -24,6 +24,8 @@ pub fn path_exists(path: &str) -> bool {
 }
 
 fn main() {
+    let now = Instant::now();
+
     let matches = App::new("depster")
         .version("0.0.2")
         .author("spelexander")
@@ -47,7 +49,7 @@ fn main() {
 
     let root = matches.value_of("root").unwrap_or(".");
 
-    let src = matches.value_of("src").unwrap_or("./src");
+    let src = matches.value_of("src").unwrap_or("src");
     let src = format!("{}/{}", &root, src);
 
     let extension_matcher = matches.value_of("ext").unwrap_or(r"(\.tsx$|\.ts$|\.jsx$|\.js$|\.mjs$|\.cjs$)");
@@ -61,13 +63,12 @@ fn main() {
         panic!("{} not found, doing nothing.", &src);
     }
 
-    println!("🔬  {}/{} found", &root, FILE_NAME);
     println!("🔬  scanning: {}", &src);
 
     let package: String = fs::read_to_string(package_json_path).expect("Unable to find file");
     let package: Package = serde_json::from_str(&package).expect("Unable to read file. Is it json?");
 
-    println!("📦  current packages: {} / {} dev", package.dependencies.keys().len(), package.devDependencies.keys().len());
+    println!("📦  dependencies: {} / {} dev", package.dependencies.keys().len(), package.devDependencies.keys().len());
 
     let deps: HashSet<String> = package.dependencies
         .keys()
@@ -83,7 +84,7 @@ fn main() {
         let version = package.devDependencies.get(&*dep).unwrap_or(package.dependencies.get(&*dep).unwrap()).to_owned();
 
         if dep.starts_with("@types") || package.devDependencies.contains_key(&*dep) {
-            new_dev_deps.insert(dep.to_owned(), version, );
+            new_dev_deps.insert(dep.to_owned(), version);
         } else {
             new_deps.insert(dep.to_owned(), version);
         }
@@ -93,58 +94,44 @@ fn main() {
         new_dev_deps.insert(dep.to_owned(), version.to_owned());
     }
 
-    println!("📦  used packages: {} / {} dev", &new_deps.len(), &new_dev_deps.len());
+    println!("📦  used dependencies: {}", &new_deps.len());
 
     let new_package = Package {
         dependencies: new_deps,
         devDependencies: new_dev_deps,
     };
 
-    let input = serde_json::to_string_pretty(&package).unwrap();
+    let input = serde_json::to_string_pretty(&package).expect("Could not serialise input json");
     let output = serde_json::to_string_pretty(&new_package).expect("Could not serialise output json");
 
     println!("📦  proposed changes:");
     println!("{}", diff_lines(&input, &output));
+
+    let elapsed = now.elapsed();
+    println!("⌛  done in {:.2?}", elapsed);
 }
 
 fn scan_files(path: &str, matcher: &Regex, dep_list: &HashSet<String>) -> HashSet<String> {
     let mut found_deps: HashSet<String> = HashSet::new();
     let mut check_list: HashSet<String> = dep_list.clone();
 
+    let parallel_reads = WalkDir::new(path);
+
     // loop over every file/dir and dive deeper or scan for deps
-    for source_path in fs::read_dir(path).expect("scanned directory does not exist!") {
+    for source_path in parallel_reads {
         if check_list.len() == 0 {
             break;
         }
 
         let entry = source_path.unwrap();
-        let file_name = entry
-            .path()
-            .file_name()
-            .unwrap()
-            .to_string_lossy()
-            .into_owned();
-        let file_path = format!("{}/{}", &path, &file_name);
-
-        let meta = metadata(entry.path()).unwrap();
-
-        if meta.is_dir() {
-            // recursively explore files in sub directories
-            let sub_found: HashSet<String> = scan_files(&file_path, matcher, &check_list);
-            for dep in sub_found {
-                found_deps.insert(dep.to_owned());
-                check_list.remove(&*dep);
-            }
-            continue;
-        }
-
+        let file_name = entry.path().display().to_string();
 
         // if not a valid file extension
         if !matcher.is_match(&file_name) {
             continue;
         }
 
-        let file_content: String = fs::read_to_string(file_path).expect("Unable to find file");
+        let file_content: String = fs::read_to_string(file_name).expect("Unable to find file");
 
         // if it's a match find specific dep and remove it from the master list
         for dep in check_list.clone() {
